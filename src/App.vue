37006fc36e31ba056744334d5ac80c2bb654cc83
DarioLoForti <lofortidario@gmail.com>
2024-03-26 17:16:56 +0100

    base vite

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <script>
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
 export default {
-  
+  components:{
+    Header,
+    Footer,
+  }
 }
 </script>
 <template lang="">
-  <div>
-    
-  </div>
+    <Header/>
+    <Footer/>
+    <router-view></router-view>
 </template>
-<style lang="">
-  
+<style lang="scss">
+  @use './styles/general.scss' as *;
 </style>
