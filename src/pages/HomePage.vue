fc396681c1d5fc400d213d0f5af070c7ab697f29
DarioLoForti <lofortidario@gmail.com>
2024-03-27 17:56:27 +0100

    slider

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -139,6 +139,67 @@ export default {
                         <img src="../../public/gruppo.png" alt="" class="img-fluid me-5">
                     </div>
                 </div>
+                <div class="row my-5">
+                    <div id="carouselExampleDark" class="carousel carousel-dark slide">
+                        <div class="carousel-indicators">
+                          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                          <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                        </div>
+                        <div class="carousel-inner">
+                          <div class="carousel-item active d-flex justify-content-center" >
+                            <div class="card my-5" style="width: 18rem;">
+                                <div class="card-body">
+                                  <h5 class="card-title">1</h5>
+                                  <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                  <a href="#" class="card-link">Card link</a>
+                                  <a href="#" class="card-link">Another link</a>
+                                </div>
+                              </div>
+                            <div class="carousel-caption d-none d-md-block">
+
+                            </div>
+                          </div>
+                          <div class="carousel-item d-flex justify-content-center">
+                            <div class="card my-5" style="width: 18rem;">
+                                <div class="card-body">
+                                  <h5 class="card-title">2</h5>
+                                  <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                  <a href="#" class="card-link">Card link</a>
+                                  <a href="#" class="card-link">Another link</a>
+                                </div>
+                              </div>
+                            <div class="carousel-caption d-none d-md-block">
+                              
+                            </div>
+                          </div>
+                          <div class="carousel-item d-flex justify-content-center">
+                            <div class="card my-5" style="width: 18rem;">
+                                <div class="card-body">
+                                  <h5 class="card-title">3</h5>
+                                  <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+                                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                  <a href="#" class="card-link">Card link</a>
+                                  <a href="#" class="card-link">Another link</a>
+                                </div>
+                              </div>
+                            <div class="carousel-caption d-none d-md-block">
+                              
+                            </div>
+                          </div>
+                        </div>
+                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+                          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Previous</span>
+                        </button>
+                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+                          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                          <span class="visually-hidden">Next</span>
+                        </button>
+                      </div>
+                </div>
             </div>
     </main>
 </template>
