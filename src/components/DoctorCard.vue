df93944237341f18be5badce6c1db4a16234d957
Zariy7 <nazariy.hereha@gmail.com>
2024-03-27 09:36:54 +0100

    single doctor show page

--- src/components/DoctorCard.vue
+++ src/components/DoctorCard.vue
@@ -14,18 +14,20 @@ export default {
 </script>
 <template lang="">
     <div class="col-3">
-        <div>
-            Dr. {{doctor.user.name}} {{doctor.user.surname}}
-        </div>
-        <div>
-            pratica a {{doctor.city}}.
-        </div>
-        <div>
-            Specializzato in: 
-            <span v-for="specialization, index in doctor.specializations">
-                {{specialization.name.toLowerCase()}}<span v-if="index == doctor.specializations.length-1">.</span>
-            </span>
-        </div>
+        <router-link :to="{name: 'doctor', params: {slug: doctor.slug}}">
+            <div>
+                Dr. {{doctor.user.name}} {{doctor.user.surname}}
+            </div>
+            <div>
+                pratica a {{doctor.city}}.
+            </div>
+            <div>
+                Specializzato in: 
+                <span v-for="specialization, index in doctor.specializations">
+                    {{specialization.name.toLowerCase()}}<span v-if="index == doctor.specializations.length-1">.</span>
+                </span>
+            </div>
+        </router-link>
     </div>
 </template>
 <style lang="scss" scoped>
---
+++ src/pages/DoctorPage.vue
@@ -0,0 +1,48 @@
+<script>
+import { store } from '../store.js';
+import axios from 'axios';
+
+export default {
+    name: 'DoctorPage',
+    components: {
+
+    },
+    data(){
+        return{
+            store,
+            doctor: null,
+        }
+    },
+    created(){
+        this.getDoctor();
+    },
+    methods: {
+        getDoctor(){
+            axios.get(`${this.store.baseUrl}/api/doctor/${this.$route.params.slug}`).then((response)=>{
+                this.doctor = response.data.response;
+            })
+        }
+    }
+}
+</script>
+<template lang="">
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center my-4">
+                    <h1>BDoctors; Be Doctors!</h1>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    Dr. {{ this.doctor.user.name }} {{ this.doctor.user.surname }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<style lang="">
+    
+</style>
