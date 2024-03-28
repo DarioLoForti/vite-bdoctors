82a2fc130a508bda0d8eab05f6d3f13060eb0cbb
DarioLoForti <lofortidario@gmail.com>
2024-03-28 10:35:43 +0100

    image jumbotron

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -30,14 +30,14 @@ export default {
     <main>
         <div class="jumbotron d-none d-md-block">
             <div class="my-container d-flex justify-content-between">
-                <img class='ms-5' src="../../public/doctor-2.png" alt="">
-                    <div>
+                <img class='image' src="../../public/doctor-5.png" alt="" >
+                    <div class="pt-5">
                         <h1 class="fs-1 my-text">Il tuo dottore a portata di un Click!</h1>
                         <p class=" text-light fs-5">Filtra le tue ricerche per città e/o specializzazioni. <br>
                             Leggi le recensioni, osserva i profili e scegli il meglio per te!
                         </p>
                     </div>
-                <img class='me-5' src="../../public/doctor-1.png" alt="">
+                <img class='image' src="../../public/doctor-6.png" alt="" style="height: 50%">
             </div>
         </div>
 
@@ -71,7 +71,7 @@ export default {
                             </div>
                     </div>
                 </div>
-                <div class="col-12 col-md-4">
+                <div class="col-12 col-md-4 d-none d-md-block">
                     <img class="me-5" src="../../public/doctor-4.png" alt="">
                 </div>
             </div>
@@ -153,7 +153,7 @@ export default {
                         <div id="carouselExample" class="carousel slide" data-bs-ride="carousel" data-bs-interval="5000">
                             <div class="carousel-inner">
                                 <div class="carousel-item active">
-                                    <div class="card" style="width: 700px;" >
+                                    <div class="card" style="width: 100%;" >
                                         <div class="card-body">
                                             <h5 class="card-title">Navigazione semplice e veloce</h5>
                                             <p class="card-text">Ho trovato il sito estremamente facile da usare per trovare un dottore specialista adatto alle mie esigenze. La barra di ricerca intuitiva e la possibilità di filtrare i risultati in base alla specializzazione, alla posizione e alle recensioni dei pazienti hanno reso il processo di selezione rapido e senza stress. Grazie alla struttura chiara e organizzata del sito, ho potuto prenotare una visita con il dottore giusto in pochi minuti. Consiglio vivamente questo sito a chiunque cerchi un modo semplice e conveniente per gestire la propria salute</p>
@@ -172,7 +172,7 @@ export default {
                                     </div>
                                 </div>
                                 <div class="carousel-item">
-                                    <div class="card" style="width: 700px;" >
+                                    <div class="card" style="width: 100%;" >
                                         <div class="card-body">
                                             <h5 class="card-title">Esperienza utente impeccabile</h5>
                                             <p class="card-text">La mia esperienza con questo sito è stata estremamente positiva grazie alla sua interfaccia utente intuitiva e ben progettata. La possibilità di cercare dottori per specializzazione e posizione ha reso la ricerca molto efficiente, mentre le schede dettagliate dei dottori con informazioni chiare e recensioni dei pazienti mi hanno aiutato a prendere una decisione informata. Inoltre, il processo di prenotazione online è stato rapido e senza intoppi. Grazie a questo sito, ho trovato il dottore perfetto per le mie esigenze in pochi clic.</p>
@@ -191,7 +191,7 @@ export default {
                                     </div>
                                 </div>
                                 <div class="carousel-item">
-                                    <div class="card" style="width: 700px;" >
+                                    <div class="card" style="width: 100%;" >
                                         <div class="card-body">
                                             <h5 class="card-title">Navigazione intuitiva e personalizzata</h5>
                                             <p class="card-text">Sono rimasto impressionato dalla facilità con cui ho potuto trovare un dottore specialista tramite questo sito. La sua interfaccia utente intuitiva mi ha permesso di cercare e filtrare i risultati in base alle mie esigenze specifiche, garantendomi di trovare rapidamente il dottore più adatto a me. Inoltre, ho apprezzato la funzionalità di salvataggio dei preferiti, che mi ha permesso di tenere traccia dei dottori che mi interessavano di più per confrontarli in seguito. Grazie alla sua navigazione fluida e personalizzata, questo sito si è dimostrato un prezioso strumento nella gestione della mia salute.</p>
@@ -210,7 +210,7 @@ export default {
                                     </div>
                                 </div>
                                 <div class="carousel-item">
-                                    <div class="card" style="width: 700px;" >
+                                    <div class="card" style="width: 100%;" >
                                         <div class="card-body">
                                             <h5 class="card-title">Interfaccia user-friendly e informativa</h5>
                                             <p class="card-text">Sono rimasto estremamente soddisfatto dell'esperienza di utilizzo di questo sito. La sua interfaccia user-friendly mi ha permesso di navigare facilmente attraverso le varie opzioni di ricerca per trovare il dottore specialista di cui avevo bisogno. Le schede dettagliate dei dottori, complete di informazioni sulla loro formazione, esperienza e recensioni dei pazienti, mi hanno aiutato a prendere una decisione informata. Inoltre, apprezzo la sezione delle domande frequenti e le risorse informative che il sito offre, che mi hanno fornito ulteriori dettagli sui diversi trattamenti e procedure disponibili. Consiglio vivamente questo sito a chiunque cerchi una piattaforma intuitiva e informativa per la ricerca di un dottore specialista.</p>
@@ -229,7 +229,7 @@ export default {
                                     </div>
                                 </div>
                                 <div class="carousel-item">
-                                    <div class="card" style="width: 700px;" >
+                                    <div class="card" style="width: 100%;" >
                                         <div class="card-body">
                                             <h5 class="card-title">Ricerca rapida e accurata, prenotazione senza sforzo</h5>
                                             <p class="card-text">Ho trovato il processo di ricerca e prenotazione di un dottore tramite questo sito incredibilmente semplice e diretto. La funzionalità di ricerca rapida mi ha permesso di trovare facilmente dottori in base alla loro specializzazione e posizione, mentre i filtri aggiuntivi per valutazioni dei pazienti e disponibilità degli appuntamenti hanno affinato ulteriormente i risultati. Una volta scelto il dottore desiderato, la procedura di prenotazione è stata altrettanto semplice e senza sforzo. In breve, questo sito ha reso il processo di trovare e prenotare un dottore un'esperienza piacevole e priva di stress. Lo consiglio vivamente a chiunque cerchi una soluzione conveniente e efficiente per le proprie esigenze mediche.</p>
@@ -246,7 +246,7 @@ export default {
                                             </div>
                                         </div>
                                     </div>
-                                </div>v
+                                </div>
                             </div>
                             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
                                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -260,7 +260,6 @@ export default {
                     </div>
                 </div>
             </div>
-        
     </main>
 </template>
 <style lang="scss" scoped>
@@ -273,6 +272,9 @@ export default {
             max-width: 80%;
             margin: 0 auto;
             padding-top: 50px;
+        }
+        .image{
+            width: 25%;
         }
     }
 
