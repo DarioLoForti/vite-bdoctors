1f9d54f28acf997e9834c6d3b00689f5d12204cf
DarioLoForti <lofortidario@gmail.com>
2024-03-28 19:24:48 +0100

    rating

--- src/pages/DoctorPage.vue
+++ src/pages/DoctorPage.vue
@@ -43,16 +43,17 @@ export default {
                         <label class="form-label" for="rating">Inserisci la tua votazione</label>
                         <form action="http://127.0.0.1:8000/ratings/create" method="GET">
                             <div class="rating">
-                                <input type="radio" id="star1" name="rating" value="1" />
-                                <label for="star1" title="1 stella">&#9733;</label>
-                                <input type="radio" id="star2" name="rating" value="2" />
-                                <label for="star2" title="2 stelle">&#9733;</label>
-                                <input type="radio" id="star3" name="rating" value="3" />
-                                <label for="star3" title="3 stelle">&#9733;</label>
-                                <input type="radio" id="star4" name="rating" value="4" />
-                                <label for="star4" title="4 stelle">&#9733;</label>
+                                <input type="hidden" name="slug" :value="this.doctor.slug">
                                 <input type="radio" id="star5" name="rating" value="5" />
                                 <label for="star5" title="5 stelle">&#9733;</label>
+                                <input type="radio" id="star4" name="rating" value="4" />
+                                <label for="star4" title="4 stelle">&#9733;</label>
+                                <input type="radio" id="star3" name="rating" value="3" />
+                                <label for="star3" title="3 stelle">&#9733;</label>
+                                <input type="radio" id="star2" name="rating" value="2" />
+                                <label for="star2" title="2 stelle">&#9733;</label>
+                                <input type="radio" id="star1" name="rating" value="1" />
+                                <label for="star1" title="1 stella">&#9733;</label>
                             </div>
                             <div>
                                 <button class="btn btn-sm btn-success" type="submit">invia</button>
